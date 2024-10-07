b1136e289713d6d22e82ff1fa0c3a7ec7e444619
0e1arquitetos <141720410+0e1arquitetos@users.noreply.github.com>
2024-10-07 15:52:18 -0300

    JFF - V02
    360 of JFF - V02

---
+++ data.js
@@ -0,0 +1,491 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall",
+      "name": "HALL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.9870499497739198,
+        "pitch": 0.09751830422023744,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7411413701248364,
+          "pitch": 0.06036036457343563,
+          "rotation": 0,
+          "target": "4-lavabo"
+        },
+        {
+          "yaw": -1.592687711922725,
+          "pitch": 0.05123395282386767,
+          "rotation": 0,
+          "target": "5-circulao"
+        },
+        {
+          "yaw": 2.4362262772987586,
+          "pitch": 0.08990143401862483,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": -2.607436745600701,
+          "pitch": 0.1628936906563201,
+          "rotation": 0,
+          "target": "2-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sala",
+      "name": "SALA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.18661627875815157,
+        "pitch": 0.018495033266063388,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4348975015572325,
+          "pitch": -0.008866594954962892,
+          "rotation": 0,
+          "target": "2-cozinha"
+        },
+        {
+          "yaw": -0.8956637987485205,
+          "pitch": 0.08286648175985789,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cozinha",
+      "name": "COZINHA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.18020306922417184,
+        "pitch": 0.011640991526277844,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0172582897816618,
+          "pitch": 0.04155446071234792,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": -1.3788490416203132,
+          "pitch": 0.13291710459890993,
+          "rotation": 0,
+          "target": "3-lavanderia"
+        },
+        {
+          "yaw": 0.26888856058062593,
+          "pitch": 0.0976452026405994,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lavanderia",
+      "name": "LAVANDERIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.841524056005488,
+        "pitch": 0.0388671980616806,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8079597282910145,
+          "pitch": 0.06476024463417929,
+          "rotation": 0,
+          "target": "2-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-lavabo",
+      "name": "LAVABO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 1.747161304602372,
+        "pitch": 0.06157703369686374,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1802316159567603,
+          "pitch": 0.30058768361115895,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-circulao",
+      "name": "CIRCULAÇÃO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.3871023973768484,
+        "pitch": 0.027633755585767972,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.307863383036068,
+          "pitch": 0.24917637751787325,
+          "rotation": 0,
+          "target": "6-escritrio"
+        },
+        {
+          "yaw": -1.7508592791454092,
+          "pitch": 0.1128545219248025,
+          "rotation": 0,
+          "target": "8-banho-social"
+        },
+        {
+          "yaw": -1.547377550458208,
+          "pitch": 0.06235893017384697,
+          "rotation": 0,
+          "target": "7-quarto-visitas"
+        },
+        {
+          "yaw": -1.3735700288549548,
+          "pitch": 0.04895944129643581,
+          "rotation": 0,
+          "target": "9-sute"
+        },
+        {
+          "yaw": 1.879013852556719,
+          "pitch": 0.05755144860972017,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-escritrio",
+      "name": "ESCRITÓRIO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.3670739233973777,
+        "pitch": 0.05517307519957626,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.26347820329344884,
+          "pitch": 0.13624848903546294,
+          "rotation": 0,
+          "target": "5-circulao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-quarto-visitas",
+      "name": "QUARTO VISITAS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.7658253987920451,
+        "pitch": 0.017230973095976765,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1794774686802132,
+          "pitch": 0.09215470329404596,
+          "rotation": 0,
+          "target": "5-circulao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-banho-social",
+      "name": "BANHO SOCIAL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.9720378953644033,
+        "pitch": 0.024056251376103788,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.048425549519256705,
+          "pitch": 0.15225532916664974,
+          "rotation": 0,
+          "target": "5-circulao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sute",
+      "name": "SUÍTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.07603355940093,
+        "pitch": 0.0008234791735315383,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23910614021228227,
+          "pitch": 0.09637884141449504,
+          "rotation": 0,
+          "target": "10-banho-sute"
+        },
+        {
+          "yaw": 1.5875264452070503,
+          "pitch": 0.0847257074703549,
+          "rotation": 0,
+          "target": "5-circulao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-banho-sute",
+      "name": "BANHO SUÍTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.3806552214647763,
+        "pitch": 0.0014647284568756191,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8597502760600815,
+          "pitch": 0.3920671315630955,
+          "rotation": 0,
+          "target": "9-sute"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "JEFF - V02",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
